c915fe3a3d7eb307a9d4036a2c371844996eef25
Josep Subils <js@js.gl>
2018-06-08 19:55:32 +0200

    [add] New eslinter file

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  rules: {
+    indent: [
+      "error",
+      2
+    ],
+    quotes: [2, "single"],
+    'comma-dangle': ["error", "never"],
+    'linebreak-style': [
+      "error",
+      "unix"
+    ],
+    quotes: [
+      "off"
+    ],
+    semi: [
+      "error",
+      "never"
+    ],
+    'no-console': [
+      "off"
+	    ],
+    'no-restricted-imports': [0]
+  }
+}

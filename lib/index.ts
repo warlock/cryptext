48735e285e1eca04e8e627cabecd09c0c25f2a15
Josep Subils <js@js.gl>
2018-08-20 12:37:44 +0200

    update

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,15 @@
-import { createCipher, createDecipher } from 'crypto'
-const algorithm = 'aes192'
-
+import { createCipheriv, createDecipheriv } from 'crypto'
+const algorithm = 'aes-256-cbc' //'aes192'
+const cipher_iv = Buffer.alloc(16)
 export = {
   encrypt(text : string, password : string) : string {
-    const cipher = createCipher(algorithm, password)
+    const cipher = createCipheriv(algorithm, new Buffer(password), cipher_iv)
     var crypted = cipher.update(text, 'utf8', 'hex')
     crypted += cipher.final('hex')
     return crypted.toString()
   },
-  decrypt(text : any, password : string) : string {
-    const decipher = createDecipher(algorithm, password)
+  decrypt(text : string, password : string) : string {
+    const decipher = createDecipheriv(algorithm, new Buffer(password), cipher_iv)
     var dec = decipher.update(text, 'hex', 'utf8')
     dec += decipher.final('utf8')
     return dec.toString()
